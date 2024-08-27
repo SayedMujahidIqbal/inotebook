f98708a329b65d2bcabe21a05bb0fcd323b104bd
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-08-27 15:15:34 +0300

    iNotebook - UpdateNote

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,75 @@
 const express = require('express')
 const router = express.Router()
+const fetchUser = require('../middleware/fetchUser')
+const Notes = require('../models/Notes')
+const { body, validationResult } = require('express-validator')
 
-router.get('/', (req, res) => {
-    res.json([])
+// ROUTE 1: Get All the notes using: GET /api/notes/fetchallnotes. Login Required
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
+
+// ROUTE 2: Create a new note using: POST /api/notes/addnote. Login Required
+router.post('/addnote', fetchUser,
+    [
+        body('title', 'Enter a valid title').isLength({ min: 5 }),
+        body('description', 'Description must be atleast 5 charatcers').isLength({ min: 5 })
+    ],
+    async (req, res) => {
+        const { title, description, tag } = req.body;
+        // If errors, return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        try {
+            const note = new Notes({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save();
+            res.json(savedNote)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Internal Server Error' })
+        }
+    })
+
+// ROUTE 3: Update Existing note using: POST /api/notes/udpatenote. Login Required
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+        // Create new note object
+        const newNote = {};
+        if(title){ newNote.title = title }
+        if(description){ newNote.description = description }
+        if(title){ newNote.tag = tag }
+        
+        //Find the note to be updated and update it
+        let note = await Notes.findById(req.params.id);
+        if(!note){ return res.status(404).send ("Not Found") }
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+
+
 
 module.exports = router
